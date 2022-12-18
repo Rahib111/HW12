c94ff2ffaeaa5e68121af9b2d68a73471a3cbf1b
= <mamedov.raxib71@gmail.com>
2022-12-18 19:27:06 +0400

    done

---
+++ HappyFamily/HappyFamily/HappyFamily/src/main/java/com/abb/dao/FamilyDao.java
@@ -0,0 +1,20 @@
+package com.abb.dao;
+
+import com.abb.model.Family;
+
+import java.util.List;
+
+public interface FamilyDao
+{
+    List<Family> getAllFamilies ();
+
+    Family getFamilyByIndex (int index);
+    Family getFamilyById (int id);
+
+    Boolean deleteFamily (int index);
+    Boolean deleteFamilyById (int id);
+
+    Boolean deleteFamily (Family family);
+
+    void saveFamily (Family family);
+}
